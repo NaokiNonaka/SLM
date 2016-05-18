6866fa54e0b9393f12d2294a0cf47c001549cdfa
NaokiNonaka <naoki@cps.im.dendai.ac.jp>
2016-05-18 19:02:48 +0900

    first commit

---
+++ Source/Functions.py
@@ -0,0 +1,57 @@
+# coding: UTF-8
+import time
+
+__author__ = 'nonakanaoki'
+
+delayval = 0.6
+delayval2 = 2
+
+
+def get_chip(var):
+    var.write(b'A0X0Y0Z200')
+    time.sleep(delayval)
+    var.write(b'A0X-108Y-46Z200')
+    time.sleep(delayval)
+    var.write(b'A0X-108Y-46Z60')
+    time.sleep(delayval)
+    var.write(b'A0X-108Y-46Z48')
+    time.sleep(delayval)
+    var.write(b'A0X-108Y-46Z200')
+    time.sleep(delayval)
+
+
+def get_reagent(var):
+    var.write(b'A0X-40Y-120Z200')
+    time.sleep(delayval)
+    var.write(b'A0X-40Y-120Z20')
+    time.sleep(delayval)
+    var.write(b'p5')
+    time.sleep(delayval2)
+    var.write(b'A0X-40Y-120Z200')
+    time.sleep(delayval)
+    # var.write(b'd')
+    # time.sleep(delayval)
+
+
+def set_reagent(var):
+    var.write(b'A0X-28Y117Z200')
+    time.sleep(delayval)
+    var.write(b'A0X-28Y117Z140')
+    time.sleep(delayval)
+    var.write(b'd')
+    time.sleep(delayval2)
+    var.write(b'A0X-28Y117Z200')
+    time.sleep(delayval)
+
+def put_chip(var):
+    var.write(b'A0X85Y155Z200')
+    time.sleep(delayval)
+    var.write(b'e')
+    time.sleep(delayval)
+
+
+def go_home(var):
+    var.write(b'h')
+
+def pos(var,comm):
+    var.write(comm.encode('utf-8'))
---
+++ Source/SLM4Abtracer.py
@@ -0,0 +1,39 @@
+# coding: UTF-8
+
+__author__ = 'nonakanaoki'
+
+import serial
+from Source.Functions import *
+
+ser = serial.Serial('/dev/tty.usbmodem1411', 9600)
+
+
+
+
+while True:
+    inp = input('command\n')
+    if inp in '1':
+        get_chip(ser)
+    elif inp in '2':
+        get_reagent(ser)
+    elif inp in '3':
+        set_reagent(ser)
+    elif inp in '4':
+        put_chip(ser)
+    elif inp in '5':
+        get_chip(ser)
+        get_reagent(ser)
+        set_reagent(ser)
+        put_chip(ser)
+    elif inp in '0':
+        break
+    elif inp in 'h':
+        go_home(ser)
+    elif inp in 'A':
+        pos(ser,inp)
+
+
+ser.close()
+
+
+
